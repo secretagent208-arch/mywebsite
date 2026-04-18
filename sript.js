b63359c4f53aed5bf5d2359170ca140dfb41ae17
secretagent208-arch <secretagent208@gmail.com>
2026-04-18 13:27:39 +0600

    Update sript.js

--- sript.js
+++ sript.js
@@ -1,7 +1,8 @@
 function buyNow(product) {
   let phone = "8801322911626";
-  let message = "Buy Now: " + product;
-  let url = "https://wa.me/" + phone + "?text=" + encodeURIComponent(message);
+  let message = "I want to buy: " + product;
+
+  let url = "https://api.whatsapp.com/send?phone=" + phone + "&text=" + encodeURIComponent(message);
 
   window.location.href = url;
 }
