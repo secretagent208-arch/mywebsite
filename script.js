c7832ba0d544bb163ed03a7515e0f276f3d7c095
secretagent208-arch <secretagent208@gmail.com>
2026-05-10 22:07:10 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,84 @@
 let cart = [];
+function addToCart(name, price) {
 
-window.addEventListener("DOMContentLoaded", function () {
+  let existing = cart.find(item => item.name === name);
 
-  window.addToCart = function (name, price) {
-
-    cart.push({ name, price });
-
-    console.log("clicked");
-
-    document.getElementById("cartCount").textContent = cart.length;
-
-    alert(name + " added to cart!");
-
-    displayCart();
-  };
-
-  function displayCart() {
-
-    let cartItems = document.getElementById("cartItems");
-
-    let total = 0;
-
-    cartItems.innerHTML = "";
-
-    cart.forEach(item => {
-
-      cartItems.innerHTML += `<p>${item.name} - ${item.price} TK</p>`;
-
-      total += item.price;
-
-    });
-
-    document.getElementById("total").innerText =
-      "Total: " + total + " TK";
+  if (existing) {
+    existing.qty += 1;
+  } else {
+    cart.push({ name, price, qty: 1 });
   }
 
-});
+  saveCart();
+  displayCart();
+
+  document.getElementById("cartCount").textContent = getTotalItems();
+}
+function getTotalItems() {
+  let total = 0;
+
+  cart.forEach(item => {
+    total += item.qty;
+  });
+
+  return total;
+}
+function displayCart() {
+
+  let cartItems = document.getElementById("cartItems");
+  let total = 0;
+
+  cartItems.innerHTML = "";
+
+  cart.forEach((item, index) => {
+
+    cartItems.innerHTML += `
+      <p>
+        ${item.name} - ${item.price} TK
+        (x${item.qty})
+
+        <button onclick="increaseQty(${index})">+</button>
+        <button onclick="decreaseQty(${index})">-</button>
+      </p>
+    `;
+
+    total += item.price * item.qty;
+  });
+
+  document.getElementById("total").innerText =
+    "Total: " + total + " TK";
+}
+function increaseQty(index) {
+  cart[index].qty += 1;
+  saveCart();
+  displayCart();
+}
+
+function decreaseQty(index) {
+  cart[index].qty -= 1;
+
+  if (cart[index].qty <= 0) {
+    cart.splice(index, 1);
+  }
+
+  saveCart();
+  displayCart();
+
+  document.getElementById("cartCount").textContent = getTotalItems();
+}
+function saveCart() {
+  localStorage.setItem("cart", JSON.stringify(cart));
+}
+
+function loadCart() {
+  let data = localStorage.getItem("cart");
+
+  if (data) {
+    cart = JSON.parse(data);
+  }
+
+  displayCart();
+  document.getElementById("cartCount").textContent = getTotalItems();
+}
+
+loadCart();
